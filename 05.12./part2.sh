121d72c1446634d6cfd5af232d492ff8b7582da4
LVGDDesman <florian.knoch@desman.xyz>
2022-12-05 13:47:02 +0100

    Day 5

---
+++ 05.12./part2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+file=$1
+[[ ! -f $file || $file == "" ]] && echo "File not found" && exit 1
+
+stacks=()
+IFS=''
+move=""
+#start
+while read line; do
+    [[ $line == "" ]] && move="1" && continue
+    if [[ $move == "" ]]
+    then
+        len=$(wc -c <<< $line)
+        i=0
+        while true; do
+            pos=$(($i*4+1)) 
+            [ $pos -gt $len ] && break;
+            char=${line:$pos:1}
+            [[ $char =~ [0-9] ]] && break
+            [[ $char == " " ]] && stacks[$i]="" || stacks[$i]="$char${stacks[$i]}"
+            i=$(($i+1))
+        done
+    else
+        count=$(sed 's/.*move \([0-9]*\).*/\1/g' <<< $line)
+        from=$(($(sed 's/.*from \([0-9]*\).*/\1/g' <<< $line)-1))
+        to=$(($(sed 's/.*to \([0-9]*\).*/\1/g' <<< $line)-1))
+        stacks[$to]+="${stacks[$from]:(-${count})}"
+        stacks[$from]="${stacks[$from]:0:(-$count)}"
+    fi
+
+done < $file
+
+sed -E 's/[a-zA-Z]*([a-zA-Z])[ ]?/\1/g' <<< "${stacks[@]}"
